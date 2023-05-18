4eb15027b4c5942b028dff06cc469088465ebb72
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-05-18 14:03:20 +0100

    Installed lottie and added lottie animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,19 @@
-import * as React from "react";
+//import Lottie from "lottie-react"
+// import animationData from "../assets/99430-statistics.json"
+//-mb-14 sm:-mb-22 md:-md-22 lg:-mb-36
 
 export default function Header():React.JSX.Element {
     return(
         <>
-        <div className="font-[Inter] flex">
-            <h1 className="font-bold pl-10 pt-10 text-5xl">Moyennes Université<br></br>Statistiques Et Visualisation Des Donées</h1>
+
+        <div className="font-[Inter] px-20 flex-col">
+            <h1 className="font-semibold text-center px-2 h-fit z-10 pt-10  text-lg sm:text-2xl md:text-4xl lg:text-5xl">Moyennes Université<br></br>Statistiques Et Visualisation Des Donées</h1>
+            {/*<div className="relative w-[90vw] mx-auto z-[-1]"><Lottie animationData={animationData}></Lottie></div>*/}
+            <h1 className="text-lg mt-14 font-medium">Sur ce projet :</h1>
+            <p>kgfhdplkgsjfhdglkjhsdfglmkjsdfhglksdfjghmsdfkjghsldkfjghsdlfkgjhsdflgkjhfgdksjhg</p>
          </div>
+
+
         </>
     )
 }
