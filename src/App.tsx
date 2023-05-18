ec0ae9c22379f8563df9a24431a7101d839bdbef
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-05-18 10:53:35 +0100

    - Complete redo - Installed lottie-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 export default function App() {
    
    return (
       <>
-         <Navbar></Navbar>
+         <Header></Header>
       </>
    );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export default function Header():React.JSX.Element {
+    return(
+        <>
+        <div className="font-[Inter] flex">
+            <h1 className="font-bold pl-10 pt-10 text-5xl">Moyennes Université<br></br>Statistiques Et Visualisation Des Donées</h1>
+         </div>
+        </>
+    )
+}
